a6b134cd6c421054b210e71c34326ddbe9b35a3f
malinda6997 <malindaprabath876@gmail.com>
2025-12-22 20:16:57 +0530

    completed login test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
 test("Valid Login Test", async function ({ page }) {
+  //Login to the application
   await page.goto(
     "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
   );
@@ -8,5 +9,13 @@ test("Valid Login Test", async function ({ page }) {
   await page.getByPlaceholder("Password").fill("admin123");
   await page.locator("//button[normalize-space()='Login']").click();
 
+  await page.waitForTimeout(5000);
   await expect(page).toHaveURL(/dashboard/);
+
+  //Logout from the application
+  await page.locator("//img[@class='oxd-userdropdown-img']").click();
+  await page.getByText("Logout").click();
+
+  await page.waitForTimeout(5000);
+  await expect(page).toHaveURL(/login/);
 });
